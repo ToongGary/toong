ec288aa2ced553542e76ce228fb0b0aeb4317bca
dlwhdtj2284 <dlwhdtj2284@naver.com>
2022-10-27 03:54:53 +0900

    Create GariHome.tsx

---
+++ src/components/pixel-painter/GariHome.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+export const TwoHome = () => <div><nav><Link to='./ToongHome'><button>gari</button></Link></nav></div>
+
+
+//수정필요
