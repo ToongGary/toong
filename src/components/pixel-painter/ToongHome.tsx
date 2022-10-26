a92e5cf92221079ca1a72c0fad600f368d2768d4
dlwhdtj2284 <dlwhdtj2284@naver.com>
2022-10-27 03:54:34 +0900

    Create ToongHome.tsx

---
+++ src/components/pixel-painter/ToongHome.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+export const OneHome = () => <div><nav> <Link to='./GariHome'><button>Toong</button></Link></nav></div>
+
+
+//수정필요
