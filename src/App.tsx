7648239927a16500e06025845d3349706bd7d77c
LeeShinYeoung <iqrash12@gmail.com>
2022-10-29 17:06:30 +0900

    Test Phaser

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,29 @@ import ColorPalette from '@components/pixel-painter/ColorPalette'
 import ColorPicker from '@components/pixel-painter/ColorPicker'
 import Reset from '@components/pixel-painter/Reset'
 import '@styles/common.css'
+import Phaser from 'phaser'
 
 function App() {
+  const game: any = new Phaser.Game({
+    width: 100,
+    height: 100,
+    type: Phaser.AUTO,
+    parent: '',
+    scene: {
+      preload: preload,
+      create: create
+      // update: update
+    }
+  })
+
+  function preload(this: any) {
+    this.load.image('logo', 'assets/background.png')
+  }
+
+  function create(this: any) {
+    this.add.image(100, 100, 'logo')
+  }
+
   return (
     <div className="pixel-painter">
       <div>
