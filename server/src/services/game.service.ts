08c71b006e05fcc97ff9e5322702ab6b761df683
LeeShinYeoung <iqrash12@gmail.com>
2022-11-08 01:20:01 +0900

    Refactor method access modifiers

--- server/src/services/game.service.ts
+++ server/src/services/game.service.ts
@@ -8,24 +8,24 @@ export class GameService {
     this.Players = new Map()
   }
 
-  join(id: string, name: string) {
+  public join(id: string, name: string) {
     this.Players.set(id, new Player(name, 100, 100, id))
   }
 
-  leave(id: string) {
+  public leave(id: string) {
     this.Players.delete(id)
   }
 
-  userInput(id: string, input: InputMessage) {
+  public userInput(id: string, input: InputMessage) {
     this.Players.get(id)?.processInput(input)
   }
 
-  update(id: string) {
+  public update(id: string) {
     const player = this.Players.get(id)
     player?.update()
   }
 
-  calculatePosition(id: string) {
+  public calculatePosition(id: string) {
     const player = this.Players.get(id)
 
     const enemies = [...this.Players.values()]
